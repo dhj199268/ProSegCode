f9505aebf2390bd388a6ba2d8089cfa7b3dbcfcf
huangjian <949442683@qq.com>
2016-11-02 22:38:07 +0800

    show img

--- src/main/Main.py
+++ src/main/Main.py
@@ -1,16 +1,19 @@
 # encoding:utf-8
 from PyQt4 import QtGui, QtCore
-from src.main.UI.MainWindow import Ui_MainWindow
+from UI.MainWindow import Ui_MainWindow
 import logging
 import sys, os
 import pyqtgraph as pg
 import numpy as np
+from Prostate.Until.Tool import loadMat
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
     def _fromUtf8(s):
         return s
+
+tmpname = "temp_data"
 
 
 class Window(QtGui.QMainWindow, Ui_MainWindow):
@@ -24,7 +27,6 @@ class Window(QtGui.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         # init Widgets
-
         ##更改视图窗口
         self.segView = pg.GraphicsView(self.centralwidget)
         self.segView.setGeometry(QtCore.QRect(20, 20, 281, 261))
@@ -47,8 +49,6 @@ class Window(QtGui.QMainWindow, Ui_MainWindow):
         self.groundImgVB.setAspectLocked()
         self.groundImgVeiw.setCentralItem(self.groundImgVB)
 
-
-
         ## 防止窗口最大化，和变动
         self.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint)
         self.setFixedSize(self.width(), self.height())
@@ -59,16 +59,42 @@ class Window(QtGui.QMainWindow, Ui_MainWindow):
     def singalHander(self):
         # singal process
         logging.info("singal process,register singal slot.")
-        # QtCore.QObject.connect(self.OpenImge, QtCore.SIGNAL('clicked()'), self.openImg)
+        QtCore.QObject.connect(self.LoadTrainDataButton, QtCore.SIGNAL('clicked()'), self.openTreatImg)
+        QtCore.QObject.connect(self.menuFile, QtCore.SIGNAL('clicked()'), self.openTreatImg)
 
-    def showImge(self):
-        logging.info("show image")
-        # QtGui.QMessageBox.information(self, u"sdf", u"sdf")
+    def openTreatImg(self):
+        self._openImg(self.imgVB)
 
-    def openImg(self):
-        filename = QtGui.QFileDialog.getOpenFileName(self, 'Open file', "Img Files (*.mat)")
-        if filename != "":
+    def openGroundTruthImg(self):
+        self._openImg(self.groundImgVB)
+
+    def openResultImg(self):
+        self._openImg(self.segVB)
+
+    def _openImg(self, viewbox):
+        filename = str(QtGui.QFileDialog.getOpenFileName(self, 'Open file'))
+        if filename:
             logging.info("open img file path:{}".format(filename))
+            img3D = loadMat(filename, tmpname)
+            img2D = get2DSlice(img3D, 13)
+            showImge(viewbox, img2D)
+            return img3D
+
+
+def showImge(viewbox, img):
+    if isinstance(viewbox, pg.ViewBox):
+        showImg = pg.ImageItem(img)
+        viewbox.addItem(showImg)
+        viewbox.autoRange()
+    else:
+        raise TypeError("viewbox arg type is error")
+
+
+def get2DSlice(img, slice):
+    if img.ndim == 3 and isinstance(img, np.ndarray):
+        return img[:, :, slice]
+    else:
+        raise TypeError("img type is error")
 
 
 if __name__ == '__main__':
